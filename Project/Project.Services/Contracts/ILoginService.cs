f72a456e21703ada43d0dbffdb003db4a418ca42
Mehmet Ali <mhmtal989@gmail.com>
2025-04-22 14:32:16 +0300

    Login Guncelleme

--- Project/Project.Services/Contracts/ILoginService.cs
+++ Project/Project.Services/Contracts/ILoginService.cs
@@ -8,5 +8,5 @@ public interface ILoginService
     Task<LoginDto> GetLoginById (int id , bool trackChanges);
     Task UpdateLogin (int id,LoginDtoUpdate loginDto,bool trackChanges);
     Task<LoginDto> CreateLogin (LoginDtoInsertion loginDto);
-    Task<LoginResponseDto> AuthenticateUser(LoginDto loginDto);
+    Task<LoginResponseDto> AuthenticateUser(LoginAuthenticationDto loginDto);
 }
