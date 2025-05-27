ac8b0e33af2e0aab243807d1da945c4b8cf73593
furkankuzucuk <149717946+furkankuzucuk@users.noreply.github.com>
2025-05-27 14:16:04 +0300

    stashede changes

---
+++ Project/Migrations/20250514094222_AddJobIdToJobListFixed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    public partial class AddJobIdToJobListFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // İşlemler kaldırıldı, çünkü zaten var.
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Geri alma işlemi yok, çünkü Up metodunda işlem yapılmadı.
+        }
+    }
+}
